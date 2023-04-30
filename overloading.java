56fd37c189298e5e5d885853aaf27cb66b71cbf7
ankit-27-1 <ankitjha2701@gmail.com>
2023-04-30 12:43:56 +0530

    Create overloading.java

---
+++ overloading.java
@@ -0,0 +1,13 @@
+public class ques1{
+    public static void main(String[] args) {
+        fun(7);
+    }
+    static void fun(int a){
+        System.out.println("first");
+        System.out.println(a);
+    }
+    static void fun(String b){
+        System.out.println("second");
+        System.out.println(b);
+    }
+}
